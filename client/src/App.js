b2ef8a9025eca314fa2c0537f190678fa1ee24eb
momaki9 <mostafa_m9@yahoo.com>
2023-02-04 10:35:51 -0800

    set up chakra for style

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,51 @@
+import React from 'react';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ChakraProvider,
+  theme,
+} from '@chakra-ui/react';
+import Home from './pages/Home';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div>
-      <h1>Test</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <ChakraProvider theme={theme}>
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={<Home />}
+            />
+          </Routes>
+        </Router>
+      </ChakraProvider>
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import { Center } from '@chakra-ui/react'
+const Home = () => {
+    return (
+        <>
+            <Center bg='tomato' h='100px' color='white' fontSize={'xx-large'}>
+                Coding Bootcamp Notes
+            </Center>
+        </>
+    )
+};
+
+export default Home;
